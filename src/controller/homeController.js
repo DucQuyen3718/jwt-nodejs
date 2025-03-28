5a03741ee00166b1bcc664e565427a51e51d5e6f
DucQuyen3718 <phamduc3718@gmail.com>
2025-03-28 13:16:59 +0700

    #5.1 - hash user password

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,12 +1,6 @@
-// Get the client
-import mysql from 'mysql2';
+import userService from '../service/userService'
 
-// Create the connection to database
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'jwt',
-});
+
 
 const handleHelloWorld = (req, res) => {
     return res.render('home.ejs')
@@ -21,21 +15,8 @@ const handleCreateNewUser = (req, res) => {
     let password = req.body.password;
     let username = req.body.username;
 
-
-
-
-
-
-    connection.query(
-        `INSERT INTO users (email, password, username) VALUES (?, ?, ?)`, [email, password, username],
-        function (err, results, fields) {
-            if (err) {
-                console.log(err)
-            }
-            console.log(results); // results contains rows returned by server
-        }
-    );
-
+    // userService.createNewUser(email, password, username);
+    userService.getUserList();
 
     return res.send("handleCreateNewUser")
 }
