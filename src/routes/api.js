33fec601c76af15b1e28920aa424524de8ed84c5
DucQuyen3718 <phamduc3718@gmail.com>
2025-04-24 10:32:43 +0700

    #11.6 - JWT middleware - check quyền user

--- src/routes/api.js
+++ src/routes/api.js
@@ -3,7 +3,7 @@ const router = express.Router();
 import apiController from "../controller/apiController"
 import userController from "../controller/userController"
 import groupController from "../controller/groupController"
-
+import { checkUserJWT, checkUserPermission } from '../middleware/JWTAction'
 /**
  * 
  * @param {*} app: express app
@@ -16,16 +16,23 @@ const testMiddleWare = (req, res, next) => {
     }
     next();
 }
+
+// const checkUserLogin = (req, res, next) => {
+//     const nonSecurePaths = ['/', '/register', '/login']
+//     if(nonSecurePaths.includes(req.path)) return next();
+
+
+//     next();
+// }
+
+
 const initApiRoutes = (app) => {
-
-
     //rest api
     //GET - R; POST - C, PUT - U, DELETE - D
-    router.get("/test-api", apiController.testApi)
     router.post("/register", apiController.handleRegister)
     router.post("/login", apiController.handleLogin)
 
-    router.get("/user/read", userController.readFunc);
+    router.get("/user/read", checkUserJWT, checkUserPermission, userController.readFunc);
     router.post("/user/create", userController.createFunc);
     router.put("/user/update", userController.updateFunc);
     router.delete("/user/delete", userController.deleteFunc);
